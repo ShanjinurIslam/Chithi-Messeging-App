7dcd7c4463d65823d3fa6da2f4f8c94849f0b91c
ShanjinurIslam <spondoncsebuet@gmail.com>
2020-08-25 01:54:46 +0600

    got rid of previous design

--- Mobile Frontend/Telegram/lib/routes.dart
+++
@@ -1,8 +0,0 @@
-import 'package:Telegram/views/home_view.dart';
-import 'package:Telegram/views/landing_view.dart';
-import 'package:flutter/material.dart';
-
-final routes = {
-  //'/': (BuildContext context) => LandingView(),
-  '/': (BuildContext context) => HomeView(),
-};
--- Mobile Frontend/Telegram/lib/utils/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-final Color customBlue = Color.fromRGBO(38, 117, 236, 1);
-final Color customBlack = Color.fromRGBO(19, 19, 19, 1);
-final Color customGray = Color.fromRGBO(132, 132, 132, 1);
-final Color customGreen = Color.fromRGBO(64, 216, 26, 1);
